e7b2a7f44a34533e05e39aed37a94309e102ae25
marius <pakalnis.marius@gmail.com>
2017-05-18 18:40:32 +0300

    fixed bug when text field is blank, styled page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,9 @@ class SearchController < ApplicationController
   end
 
   def search_result
-    @list = flickr.photos.search(tags: params[:tag]).map{|info| FlickRaw.url_q(info) }.each_slice(4).to_a
+    if !params[:tag].blank?
+      @list = flickr.photos.search(tags: params[:tag]).map{|info| FlickRaw.url_q(info) }.each_slice(6).to_a
+    end
   end
 
 end
